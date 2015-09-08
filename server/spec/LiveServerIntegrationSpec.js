414da99c69e219e8e8862e9bf3a98146d85f51a1
beasta <barryblaha@gmail.com>
2015-09-08 16:25:24 -0700

    strugglin

--- server/spec/LiveServerIntegrationSpec.js
+++ server/spec/LiveServerIntegrationSpec.js
@@ -61,6 +61,7 @@ describe('server', function() {
       request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
           var messages = JSON.parse(body).results;
           // expect(messages.length).to.equal(0);
+          console.log('boobs')
           expect(messages[0].username).to.equal('Jono');
           expect(messages[0].message).to.equal('Do my bidding!');
           done();
